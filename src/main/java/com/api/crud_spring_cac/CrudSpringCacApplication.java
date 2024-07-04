19e1b5b7d38edf0b8019ba25185bf7997a202326
MartinXCVI <mdzg96@gmail.com>
2024-07-03 22:54:59 -0300

    commit inicial

---
+++ src/main/java/com/api/crud_spring_cac/CrudSpringCacApplication.java
@@ -0,0 +1,13 @@
+package com.api.crud_spring_cac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringCacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringCacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/crud_spring_cac/CrudSpringCacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.crud_spring_cac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringCacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
